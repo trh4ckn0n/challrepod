14039e62e96fe1a7b7db8c03bdc9206ff646ef74
trh4ckn0n <trhacknon@proton.me>
2025-06-25 11:23:48 +0200

    Update trap.js

--- static/trap.js
+++ static/trap.js
@@ -1,5 +1,10 @@
-fetch("/ping", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ user: navigator.userAgent })
-});
+console.log("Trap JS loaded! Sending beacon...");
+
+fetch('/ping', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ user: "trap.js" })
+})
+.then(response => response.json())
+.then(data => console.log("Ping status:", data.status))
+.catch(err => console.error("Ping error:", err));
